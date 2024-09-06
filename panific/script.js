056f3c90a431b4b19cea57ec6d8ae3e0f6743fc0
Alisson Batista <alissonbatisx@gmail.com>
2024-09-05 21:32:40 -0300

    muitas mudanças

--- panific/script.js
+++ panific/script.js
@@ -1,13 +1,41 @@
-function verNoMaps (){
-    alert("hello word")
+let count =1;
 
-    
+document.getElementById("radio1").checked = true;
+console.log(document.getElementById("radio1"));  // Verifica se o elemento existe
+
+
+
+setInterval(function() {
+    console.log("teste do set interval")
+    nextImage();
+}, 4000);
+
+function nextImage(){
+    console.log("mudando a imagem",count);
+    count++;
+    if(count>4){
+        count = 1;
+    }
+
+    document.getElementById("radio"+count).checked = true;
 }
-document.getElementById("botaoPadariaDaFeira").addEventListener("click",function() {
-                                
-    window.open("https://www.google.com/maps/place/Padaria+e+Lanchonete+Santa+Em%C3%ADlia/@-7.1608845,-34.8958804,3a,75y,204.71h,90t/data=!3m7!1e1!3m5!1sBVPIvi_KjzcIuU9XN-By0g!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3DBVPIvi_KjzcIuU9XN-By0g%26cb_client%3Dmaps_sv.tactile.gps%26w%3D203%26h%3D100%26yaw%3D204.70526%26pitch%3D0%26thumbfov%3D100!7i16384!8i8192!4m9!3m8!1s0x7ace9adf0b58b1b:0x2ab2b7202e87932e!8m2!3d-7.1610254!4d-34.8959433!10e5!14m1!1BCgIgARICCAI!16s%2Fg%2F11b67kcfd1?coh=205409&entry=ttu&g_ep=EgoyMDI0MDgyMS4wIKXMDSoASAFQAw%3D%3D ", "_blank");
-});
 
-document.getElementById("botaoPadariaDaPraca").addEventListener("click",function(){
-    window.open("https://www.google.com/maps/@-7.1609567,-34.8921311,3a,75y,201.31h,86.13t/data=!3m6!1e1!3m4!1sDNM90A1hSdGxxYThlo3QJA!2e0!7i16384!8i8192?coh=205409&entry=ttu&g_ep=EgoyMDI0MDgyMS4wIKXMDSoASAFQAw%3D%3D" , "_blank");
-})
+
+function menuShow(){
+    let menuMobile = document.querySelector('.mobileMenu');
+    let icone = document.querySelector("#iconMenu");
+
+
+    if (menuMobile.classList.contains("open")){
+        menuMobile.classList.remove("open");
+        icone.classList.remove("fa-times")
+        icone.classList.add("fa-bars")
+        
+    } else {
+        menuMobile.classList.add("open")
+        icone.classList.remove("fa-bars");
+        icone.classList.add("fa-times");
+    }
+}
+
+
